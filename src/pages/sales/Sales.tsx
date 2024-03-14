ac9036a7d3851e194e2b9ba1d69c397bd58423bc
mahat sumit <129563818+mahatsumit5@users.noreply.github.com>
2024-03-15 02:26:42 +1100

    Update Sales.tsx

--- src/pages/sales/Sales.tsx
+++ src/pages/sales/Sales.tsx
@@ -3,10 +3,10 @@ import TotalSalesChart from "./TotalSalesChart";
 export default function Sales() {
   return (
     <div className="h-fit grid col-span-1 gap-5">
-      <div className="w-full sm:mx-auto sm:w-5/6 bg-white rounded-2xl sm:p-10 min-h-96 dark:bg-black/10">
+      <div className="w-full sm:mx-auto  bg-white rounded-2xl sm:p-10 min-h-96 dark:bg-black/10">
         <TotalSalesChart />
       </div>
-      <div className="rounded-lg bg-white sm:w-5/6 mx-auto min-h-96 dark:bg-black/10 p-5 w-full">
+      <div className="rounded-lg bg-white mx-auto min-h-96 dark:bg-black/10 p-5 w-full">
         Tablel
       </div>
     </div>
